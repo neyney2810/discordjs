6555c96afe2f88d268092e4e99f37d05d0c5e5db
kanaria2810 <nguyenyen281099@gmail.com>
2022-02-13 02:25:06 +0100

    Add handle message to ping

--- commands/meta/ping.js
+++ commands/meta/ping.js
@@ -22,26 +22,33 @@ module.exports = {
         const collector = interaction.channel.createMessageComponentCollector({ filter, time: 30 * 1000 });
 
         collector.on('collect', async i => {
-            if (i.customId === 'primary') {
-				await i.deferUpdate();
-				await i.editReply({ content: `Ping: ${interaction.client.ws.ping}`, components: [row] });
-				await wait(15 * 1000);
-				await i.editReply({ content: `Ping: ${interaction.client.ws.ping}`, components: [] });
-            }
+			await i.deferUpdate();
+			await i.editReply({ content: `Ping: ${interaction.client.ws.ping}`, components: [row] });
+			await wait(15 * 1000);
+			await i.editReply({ content: `Ping: ${interaction.client.ws.ping}`, components: [] });
         });
 
         collector.on('end', collected => console.log(`Collected ${collected.size} items`));
 
-		// const embed = new MessageEmbed()
-		// 		.setColor('#0099ff')
-		// 		.setTitle('Title')
-		// 		.setURL('https://discord.js.org')
-		// 		.setDescription('Some description here')
-		// await interaction.reply({ content: 'Pong!', ephemeral: true, embeds: [embed], components: [row] })
-
 	},
 
 	handleMessage(message) {
-		message.channel.send(`Ping: ${message.client.ws.ping}ms`);
+		const row = new MessageActionRow()
+			.addComponents(
+				new MessageButton()
+					.setCustomId('primary')
+					.setLabel('Ping')
+					.setStyle('PRIMARY')
+			);
+
+		message.channel.send({ content: `Ping: ${message.client.ws.ping}ms`, components: [row], ephemeral: true })
+
+		const filter = i => i.customId === 'primary';
+        const collector = message.channel.createMessageComponentCollector({ filter, time: 60 * 1000 });
+        collector.on('collect', async i => {
+			message.channel.send({ content: `Ping: ${message.client.ws.ping}ms`, ephemeral: true });
+        });
+
+        collector.on('end', collected => console.log(`Collected ${collected.size} items`));
 	},
 };
